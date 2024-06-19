e5be7779dd86e61430f363eac23e6861747031fe
vegeta2102 <hoangvmh@gmail.com>
2024-06-19 09:52:18 +0900

    Add Timber

--- app/src/main/java/com/example/gala/Gala.kt
+++ app/src/main/java/com/example/gala/Gala.kt
@@ -2,10 +2,12 @@ package com.example.gala
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class Gala : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.d("Gala is started")
     }
 }
